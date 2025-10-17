8191cef4f591ef6cb9f1ca5116e771e300da0416
RcmSantos274 <136946716+RcmSantos274@users.noreply.github.com>
2025-10-17 11:41:17 -0300

    Update costumer.js

--- models/costumer.js
+++ models/costumer.js
@@ -1,10 +1,28 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-    _id: {type: String, required: true},
-    name: {type: String, required: true},
-    cpf: {type: String, require: true, unique: true},
-    email:{type:  String, required: true}
-}, { _id: false })
+  _id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  cpf: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  accounts: [{
+    type: String,
+    ref: 'Account'
+  }]
+}, { _id: false });
 
-module.exports = mongoose.model("Costumer", customerSchema)
+module.exports = mongoose.model('Customer', customerSchema);
